8668d50383e63675eacbc2c63c6fd0a5c9bcf604
yoher <yoher.obando.duarte@gmail.com>
2025-11-26 00:18:16 -0600

    build configuration

--- src/core/database.py
+++ src/core/database.py
@@ -1,3 +1,4 @@
+# src/core/database.py
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 import ssl
@@ -5,15 +6,22 @@ from src.core.config import settings
 
 ssl_context = ssl.create_default_context()
 ssl_context.check_hostname = False
-ssl_context.verify_mode = ssl.CERT_NONE  # Cambia según tu setup
+ssl_context.verify_mode = ssl.CERT_NONE
 
 engine = create_async_engine(
     settings.DATABASE_URL,
-    connect_args={"ssl": ssl_context},  # <- aquí evitamos sslmode en URL
+    connect_args={"ssl": ssl_context},
     future=True,
     echo=True,
 )
 
 AsyncSessionLocal = sessionmaker(
-    bind=engine, class_=AsyncSession, expire_on_commit=False
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
 )
+
+
+async def get_db() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
