c3a6fbe7e735dfcf8190ba044173760353427445
yoher <yoher.obando.duarte@gmail.com>
2025-11-25 22:29:11 -0600

    build configuration

--- src/core/config.py
+++ src/core/config.py
@@ -1,27 +1,26 @@
 from pydantic_settings import BaseSettings
 from pydantic import Field
 from functools import lru_cache
+import re
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = Field(..., env="PROJECT_NAME")
-    POSTGRES_HOST: str = Field(..., env="POSTGRES_HOST")
-    POSTGRES_PORT: int = Field(..., env="POSTGRES_PORT")
-    POSTGRES_USER: str = Field(..., env="POSTGRES_USER")
-    POSTGRES_PASSWORD: str = Field(..., env="POSTGRES_PASSWORD")
-    POSTGRES_DB: str = Field(..., env="POSTGRES_DB")
+    ENV: str = Field("production", env="ENV")
+    DATABASE_URL: str = Field(..., env="DATABASE_URL")
+    DB_DRIVER: str = Field("psycopg", env="DB_DRIVER")
+    API_PORT: int = Field(8000, env="API_PORT")
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
 
     @property
-    def DATABASE_URL(self) -> str:
-        return (
-            f"postgresql+psycopg://{self.POSTGRES_USER}:"
-            f"{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:"
-            f"{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+    def SQLALCHEMY_DATABASE_URL(self) -> str:
+        url = re.sub(
+            r"^postgresql://", f"postgresql+{self.DB_DRIVER}://", self.DATABASE_URL
         )
+        return url
 
 
 @lru_cache()
